e52d8609497aebc3db7f5d8f266e7f0000ef4600
Alain Sirois <alainsirois@gmail.com>
2013-09-07 20:10:15 -0400

    player views stylesheet controller

--- pool/app/controllers/players_controller.rb
+++ pool/app/controllers/players_controller.rb
@@ -69,6 +69,6 @@ class PlayersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def player_params
-      params.require(:player).permit(:name, :points, :goals, :assists, :rank, :nhl_points, :nhl_goals, :nhl_assists, :nhl_rank, :team, :last_team, :power_play, :pp_last_year, :position, :salary)
+      params.require(:player).permit(:name, :points, :goals, :assists, :rank, :nhl_points, :nhl_goals, :nhl_assists, :nhl_rank, :team, :last_team, :power_play, :pp_last_year, :position, :salary, :color, :games, :drafted)
     end
 end
