c173b47d2030197ad0e9c7053ae4004d3d9db12b
Alain Sirois <alainsirois@gmail.com>
2013-09-08 02:28:36 -0400

    multiple edit patch et edit normal patch aussi

--- pool/app/controllers/players_controller.rb
+++ pool/app/controllers/players_controller.rb
@@ -1,6 +1,5 @@
 class PlayersController < ApplicationController
-  #before_action :set_player, only: [:show, :edit, :update, :destroy]
-  #before_action :set_player, only: [:show, :edit, :update, :destroy, :edit_individual, :update_individual]
+  before_action :set_player, only: [:show, :edit, :update, :destroy]
   
   # GET /players
   # GET /players.json
