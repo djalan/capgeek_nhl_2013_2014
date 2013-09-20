acacaf14c78cc5e958ecd733d2224849aa39a7ad
Alain Sirois <alainsirois@gmail.com>
2013-09-19 22:19:04 -0400

    select box for color and drafted

--- pool/app/models/player.rb
+++ pool/app/models/player.rb
@@ -1,28 +1,32 @@
 class Player < ActiveRecord::Base
+  
+  POOLERS = ['Alain', 'Ben', 'Couv', 'Mark', 'Math', 'Pat', 'no', 'yes']
+  
+  COLORS = ['green', 'yellow', 'orange', 'red', 'cyan', 'white']
 
-#  attr_accessible :name, :points, :goals, :assists, :rank, :nhl_points, :nhl_goals, :nhl_assists, :nhl_rank, :team, :last_team, :power_play, :pp_last_year, :position, :salary, :color
+
 
   validates :name, :points, :goals, :assists, :rank, :games, :nhl_points, :nhl_goals, :nhl_assists, :nhl_rank, :position, :salary,
-    presence: true
+  presence: true
 
   validates :points, :goals, :assists, :rank, :games, :nhl_points, :nhl_goals, :nhl_assists, :nhl_rank, :salary,
-    numericality: {greater_than_or_equal_to: 0}
+  numericality: {greater_than_or_equal_to: 0}
 
   validates :rank, :nhl_rank,
-    uniqueness: true
+  uniqueness: true
 
   validates :position,
-    format: {
-      with: /D|C|L|R|G/i, 
-      message: 'must be D C L R G'
-    }
+  format: { with: /D|C|L|R|G/i, message: 'must be D C L R G' }
 
   validates :power_play, :pp_last_year,
-    allow_blank: true,
-    format: {
-      with: /F1|F2|F3|F4|D1|D2|D3/i, 
-      message: 'must be F1 F2 F3 F4 D1 D2'
-    }
+  allow_blank: true, format: { with: /F1|F2|F3|F4|D1|D2|D3/i, message: 'must be F1 F2 F3 F4 D1 D2' }
+    
+  validates :color,
+  inclusion: COLORS
+    
+  validates :drafted,
+  inclusion: POOLERS
+    
 
 
   #validate :points_equal_sum_goals_assists, :nhl_points_equal_sum_nhl_goals_nhl_assists
